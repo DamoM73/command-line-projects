dd614e55b4a048a09cd056a2f68f43b41c58b02e
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2022-04-19 23:11:16 +1000

    wordle

---
+++ wordle.py
@@ -0,0 +1,61 @@
+import random
+
+def get_word():
+    # open word list
+    with open("dictionary.txt", "r") as word_file:
+        words = word_file.read().splitlines()
+    # choose random word
+    while True:
+        word = random.choice(words)
+        if len(word) == 5:
+            return word.upper()
+        
+def word_to_list(word):
+    temp = []
+    for letter in word:
+        temp.append(letter)
+    return temp
+
+def get_guess():
+    while True:
+        guess = input("> ").upper()
+        if len(guess) == 5:
+            return guess
+   
+
+# ----- MAIN PROGRAM ----- #
+
+# --- Game Variables --- #
+word = word_to_list(get_word())
+
+print(word)
+guesses = []
+running = True
+
+# --- Main Loop --- #
+while running:
+    guess = word_to_list(get_guess())
+    
+    # check for correct guess 
+    if guess == word:
+        print("\nWINNER!")
+        running = False
+    else:
+        temp = []
+        for index in range(5):
+            if guess[index] == word[index]:
+                temp.append("O")
+            elif guess[index] in word:
+                temp.append("?")
+            else:
+                temp.append("X")
+        guesses.append(temp)
+        
+        # print guesses
+        for line in guesses:
+            print("".join(line))
+            
+        # check for loss
+        if len(guesses) == 6:
+            print("Bad Luck")
+            running = False
