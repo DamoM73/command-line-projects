348f93d4880ebde77b9080cb12b4d4ef87981dea
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2022-04-22 07:38:13 +1000

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,3 +1,6 @@
+from tkinter import E
+
+
 def main_menu():
     print("Do you wish to encrypt or decrypt or quit?")
     while True:
@@ -8,7 +11,27 @@ def main_menu():
 
 def get_message():
     while True:
-        message = input("Enter message > ")
+        message = input("Enter message > ").upper()
+        if message.replace(" ","").isalpha():
+            return message
+        print("Only letters and spaces allowed.\n")
+
+
+def get_key():
+    while True:
+        key = input("Enter key > ")
+        if key.isdigit():
+            key = int(key)
+            return key % 26
+        print("Please eneter a number.\n")
+        
+def shift_letters(message, key):
+    new_message = ""
+    for char in message:
+        pos = SYMBOLS.index(char)
+        new_message = new_message + SYMBOLS[(pos + key)%26]
+    return new_message
+        
     
 # ----- MAIN PROGRAM ----- #
 
@@ -25,4 +48,7 @@ while option != "Q":
     
     # encoding
     if option == "E":
-        
+        message = get_message()
+        key = get_key()
+        print(shift_letters(message,key))
+            
