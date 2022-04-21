b92bcef22a9d38acf471a56df8935ac5fafbec22
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2022-04-21 12:48:00 +1000

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -5,6 +5,10 @@ def main_menu():
         if response in ("E","D","Q"):
             return response
     
+
+def get_message():
+    while True:
+        message = input("Enter message > ")
     
 # ----- MAIN PROGRAM ----- #
 
@@ -18,4 +22,7 @@ print("Spaces can be used to distinguish words.\n")
 option = ""
 while option != "Q":
     option = main_menu()
+    
     # encoding
+    if option == "E":
+        
